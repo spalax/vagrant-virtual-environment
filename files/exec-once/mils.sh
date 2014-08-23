705feaf4e14ed8a21227bb9c2928d0e8365ec655
Oleksii Mylotskyi <spalaxinco@gmail.com>
2014-08-24 00:07:51 +0300

    Updated first run script

--- files/exec-once/mils.sh
+++ files/exec-once/mils.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-confFile="/etc/nginx/sites-enabled/awesome.dev.conf"
+confDir="/etc/nginx/sites-enabled"
+sudo rm $confDir/_.conf
 
 hostType=$(/bin/grep 'type' /vagrant/.Vagrantfilerc.yaml | awk '{match($0,"type:\\s+(.*?)\\s*",a)}END{print a[1]}');
 
-servername=$(/bin/grep server_name $confFile --color=none -h |  awk '{match($0,"server_name\\s+(.*?)\\s*;",a)}END{print a[1]}');
-rootdir=$(/bin/grep root $confFile --color=none -h | awk '{match($0,"root\\s+(.*?)\\s*;",a)}END{print a[1]}' | awk '{gsub("\/", "\\\\&");print}');
-fastcgipass=$(/bin/grep fastcgi_pass $confFile --color=none -h |  awk '{match($0,"fastcgi_pass\\s+(.*?)\\s*;",a)}END{print a[1]}');
+servername=$(/bin/grep server_name $confDir/* --color=none -h |  awk '{match($0,"server_name\\s+(.*?)\\s*;",a)}END{print a[1]}');
+rootdir=$(/bin/grep root $confDir/* --color=none -h | awk '{match($0,"root\\s+(.*?)\\s*;",a)}END{print a[1]}' | awk '{gsub("\/", "\\\\&");print}');
+fastcgipass=$(/bin/grep fastcgi_pass $confDir/* --color=none -h |  awk '{match($0,"fastcgi_pass\\s+(.*?)\\s*;",a)}END{print a[1]}');
 
 echo $fastcgipass;
 echo $rootdir;
 echo $servername;
 
 destFile="/etc/nginx/sites-available/$hostType";
+sudo rm $confDir/*;
 
 sudo cp -f /vagrant/puphpet/files/resources/nginx/vhost/$hostType $destFile && cd /etc/nginx/sites-enabled && sudo ln -s ../sites-available/$hostType; 
 
 sudo sed -i -e "s/\${servername}/$servername/" -e "s/\${rootdir}/$rootdir/" -e "s/\${fastcgipass}/$fastcgipass/" $destFile
-sudo rm $confFile
-sudo rm /etc/nginx/sites-enabled/_.conf
 sudo /etc/init.d/nginx restart
 
 if [ "$hostType" == "magento" ] 
